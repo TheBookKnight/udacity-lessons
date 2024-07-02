9beaf6f85af2c1b5d7428f0b055e7b789290dae8
TheBookKnight <cadav001@umn.edu>
2024-07-02 18:26:03 -0400

    5 - Finding index of max value of 1d array

--- Machine Learning for Trading/3 The Power of Numpy/practice-numpy.py
+++ Machine Learning for Trading/3 The Power of Numpy/practice-numpy.py
@@ -14,8 +14,21 @@ def get_min_and_max_along_row_and_column():
     print("Mean of all elements:\n", a.mean())  # leave out the axis arg
 
 
+def get_max_index(a):
+    """Return index of the maximum value in given 1D array."""
+    return a.argmax()
+
+
 def test_run():
     get_min_and_max_along_row_and_column()
+
+    a = np.array([9, 6, 2, 3, 12, 14, 7, 10],
+                 dtype=np.int32)  # 32-bit integer array
+    print("Array:", a)
+
+    # Find the maximum and its index in array
+    print("Maximum value:", a.max())
+    print("Index of max.:", get_max_index(a))
 
 
 if __name__ == "__main__":
