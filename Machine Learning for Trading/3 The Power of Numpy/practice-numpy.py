c2fed09c0f4c10c26c1c4d79ea34db428c29fdf3
TheBookKnight <cadav001@umn.edu>
2024-07-02 18:24:09 -0400

    5 - Refactor

--- Machine Learning for Trading/3 The Power of Numpy/practice-numpy.py
+++ Machine Learning for Trading/3 The Power of Numpy/practice-numpy.py
@@ -3,15 +3,19 @@
 import numpy as np
 
 
-def test_run():
+def get_min_and_max_along_row_and_column():
     np.random.seed(693)  # seed the random number generator
     a = np.random.randint(0, 10, size=(5, 4))  # 5x4 random integers in [0, 10]
     print("Array:\n", a)
 
-    # Sattistics: min, max, mean (across rows, cols, and overall)
+    # Statistics: min, max, mean (across rows, cols, and overall)
     print("Minimum of each column:\n", a.min(axis=0))
     print("Maximum of each row:\n", a.max(axis=1))
     print("Mean of all elements:\n", a.mean())  # leave out the axis arg
+
+
+def test_run():
+    get_min_and_max_along_row_and_column()
 
 
 if __name__ == "__main__":
